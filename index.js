7a5d1e700553b385199d27b248d496824ed9bd09
Connor Johnson <connorjohnson@connors-air.mynetworksettings.com>
2024-03-26 03:15:36 -0400

    added functions to display the tables upon the users request. used connection within index.js

--- index.js
+++ index.js
@@ -23,22 +23,86 @@ const questions = [
     }
 ]
 
+function viewAllDepartments(){
+    connection.query("SELECT * FROM department", function (err, data) {
+        console.table(data);
+    })
+}
+
+function viewAllRoles(){
+    connection.query("SELECT * FROM role", function (err, data) {
+        console.table(data);
+    })
+}
+
+function viewAllEmployees(){
+    connection.query("SELECT * FROM employee", function (err, data) {
+        console.table(data);
+    })
+}
+
+function addDepartment(){
+    inquirer.prompt([{
+        type: "input",
+        name: "department",
+        message: "Please enter the department you would like to add: "
+    }, ]).then(function(res) {
+        connection.query('INSERT INTO department (department_name) VALUES (?)', [res.department], function(err, data) {
+            if (err) throw err;
+            console.table("The new department has been added.");
+            connection.query("SELECT * FROM department", function (err, data) {
+                console.table(data);
+            })
+        })
+    })
+}
+
+function addRole(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "role",
+            message: "Please enter the role you would like to add: "
+        }, 
+        {
+            type: "input",
+            name: "salary",
+            message: "Please enter the salary of the role: "
+        },
+        {
+            type: "input",
+            name: "dept_id",
+            message: "Please enter the department id of the role: "
+        }
+    ]).then(function(res) {
+        connection.query('INSERT INTO role (title, salary, dept_id) VALUES (?,?,?)', [res.role, res.salary, res.dept_id], function(err, data) {
+            if (err) throw err;
+            console.table("The new role has been added.");
+            connection.query("SELECT * FROM role", function (err, data) {
+                console.table(data);
+            })
+        })
+    })
+}
+
+
+
 inquirer.prompt(questions).then((responses)=>{
     console.log(responses);
     if (responses.choice === 'view all departments'){
-        
+        viewAllDepartments();
     }
     else if(responses.choice === 'view all roles'){
-        
+        viewAllRoles();
     }
     else if(responses.choice === 'view all employees'){
-        
+        viewAllEmployees();
     }
     else if(responses.choice === 'add a department'){
-        
+        addDepartment();
     }
     else if(responses.choice === 'add a role'){
-        
+        addRole();
     }
     else if(responses.choice === 'add an employee'){
         
