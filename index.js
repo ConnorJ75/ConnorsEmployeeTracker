195b28a0e1cc08ded7b8543062423432b0d07f88
Connor Johnson <connorjohnson@connors-air.mynetworksettings.com>
2024-03-26 02:47:43 -0400

    added basic inquirer functionality as well as some question logic. Had to downgrade to inquirer 8.2.4 in order to get things to run

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
-const { default: inquirer } = require("inquirer");
+const inquirer = require("inquirer");
+const mysql = require('mysql2');
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password123",
+    database: "employees_db"
+});
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
 
 const questions = [
     {
         type: "list",
-        name: "shape",
-        message: "What shape do you want your logo to be?(use arrow keys):",
-        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'and update an employee role']
+        name: "choice",
+        message: "What would you like to do?:",
+        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
     }
 ]
 
 inquirer.prompt(questions).then((responses)=>{
     console.log(responses);
+    if (responses.choice === 'view all departments'){
+        
+    }
+    else if(responses.choice === 'view all roles'){
+        
+    }
+    else if(responses.choice === 'view all employees'){
+        
+    }
+    else if(responses.choice === 'add a department'){
+        
+    }
+    else if(responses.choice === 'add a role'){
+        
+    }
+    else if(responses.choice === 'add an employee'){
+        
+    }
+    else if(responses.choice === 'update an employee role'){
+        
+    }
 });
